ccdafe99221d27076403aef5011cd6d095ba0c43
lakehanne <patlekano@gmail.com>
2018-08-16 18:12:21 +0900

    updated ik_sub for new joints collected

--- torobo_ik/src/joints_pub.py
+++ torobo_ik/src/joints_pub.py
@@ -12,6 +12,30 @@ from os.path import expanduser, join
 rospack = rospkg.RosPack()
 lyap = rospack.get_path('lyapunovlearner')
 
+def prepro(filename):
+    with open(filename, 'r+') as foo:
+        data = foo.readlines()
+    moplan_data = [x.split() for x in data]   
+
+    proper_data = []
+    for i in range(len(moplan_data)):
+        if not moplan_data[i]:
+            continue
+        temp = moplan_data[i]
+        to_append = []
+        temp = [x.split(',')[0] for x in temp]
+        for x in temp:
+            if '[' in x:
+                x = x.split("[")[1]
+            elif ']' in x:
+                x = x.split(']')[0]
+            to_append.append(float(x))
+        proper_data.append(to_append)
+
+    proper_data = np.array(proper_data)
+
+    return proper_data
+
 def talker(data):
     pub = rospy.Publisher('/torobo_ik/teach_joints', numpy_msg(Numpy64),queue_size=1)
     r = rospy.Rate(10) # 10hz
@@ -20,11 +44,12 @@ def talker(data):
         r.sleep()
 
 if __name__ == '__main__':
-    filepath = join(lyap, 'ToroboTakahashi', 'data')
-    name     = 'state_joint.npy'
-    #name     = 'state_joint_pos_only.npy'
+    filepath = join(lyap, 'scripts', 'data')
+    name     = 'state_joint_1.csv'
     filename = join(filepath, name)
-    data_raw = np.load(filename)
+
+    data_raw = prepro(filename)
+
     # print(data_raw.shape)
     data     =  np.ravel(data_raw, order='A')
     np.set_printoptions(suppress=True)
